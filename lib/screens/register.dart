c428e8bb820b8fa96e4c7ffb46794b7474161359
Motdep@sse <lirsitogo2021@gmail.com>
2023-01-18 09:54:26 +0000

    modified:   lib/screens/register.dart

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,143 +1,246 @@
-// ignore_for_file: avoid_print
-
-import 'dart:async';
-
-import 'package:dating/screens/home.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:dating/utils/constant.dart';
+import 'package:flukit_icons/flukit_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl_phone_field/intl_phone_field.dart';
-import 'package:lottie/lottie.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: RegisterPage(),
-  ));
-}
+import 'package:vibration/vibration.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
-  static String verify = "";
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  var phonef = "";
-  late String _dropdownValueTypeUser, username, password;
-  late bool showprogress;
-
-  @override
-  void initState() {
-    showprogress = false;
-    Firebase.initializeApp().whenComplete(() => {print("Complete!")});
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    late double screenWidth = MediaQuery.of(context).size.width;
-    late double screenHeigth = MediaQuery.of(context).size.height;
-
-    // Firebase.initializeApp();
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: primaryColorWithOpacity,
       body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
         child: Padding(
-          padding: EdgeInsets.all(screenWidth * .04),
+          padding: paddingDefault,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Lottie.asset("assets/img/date1.json"),
+            children: [
+              SizedBox(
+                height: height * .04,
               ),
-              Text(
-                "Please\nEnter your\nPhone number",
-                style: GoogleFonts.poppins(
-                    fontSize: 34, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              IntlPhoneField(
-                initialCountryCode: 'TG',
-                showDropdownIcon: false,
-                flagsButtonMargin: EdgeInsets.only(
-                    left: screenWidth * .05, right: screenWidth * .05),
-                keyboardType: TextInputType.phone,
-                onChanged: (phone) {
-                  phonef = phone.completeNumber;
-                  print(phone.completeNumber);
+              GestureDetector(
+                onTap: () {
+                  Vibration.vibrate(duration: 30, amplitude: 30);
+                  /*  Get.offAll(
+                      () => BottomBar(
+                            id_client: widget.id_client,
+                          ),
+                      transition: Transition.leftToRight,
+                      duration: Duration(milliseconds: 500)); */
                 },
-                decoration: InputDecoration(
-                  hintText: "Phone Number",
-                  hintStyle: TextStyle(fontFamily: "Poppins"),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(35),
-                      borderSide: BorderSide(color: Colors.black)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(35),
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 233, 19, 90))),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * .17,
+                  height: MediaQuery.of(context).size.height * .08,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 254, 254, 255),
+                      /* boxShadow: [
+                        BoxShadow(
+                            color: const Color(0xFF2dce89).withOpacity(.1),
+                            spreadRadius: 3,
+                            blurRadius: 12,
+                            offset: Offset(3, 3))
+                      ], */
+                      border: Border.all(
+                          width: 2,
+                          color: Color.fromARGB(193, 255, 255, 255)
+                              .withOpacity(.2)),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      )),
+                  child: FluIcon(
+                    FluIcons.arrowLeft,
+                    color: Colors.grey,
+                  ),
                 ),
               ),
               SizedBox(
-                height: 60,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    setState(() {
-                      showprogress = true;
-                      /* Timer(const Duration(seconds: 3), () {
-                         /*  Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const OtpPage())) */;
-                        }) */
-                    });
-                    await FirebaseAuth.instance.verifyPhoneNumber(
-                      phoneNumber: '{$phonef}',
-                      verificationCompleted:
-                          (PhoneAuthCredential credential) {},
-                      verificationFailed: (FirebaseAuthException e) {},
-                      codeSent: (String verificationId, int? resendToken) {
-                        RegisterPage.verify = verificationId;
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomePage()));
-                      },
-                      codeAutoRetrievalTimeout: (String verificationId) {},
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 233, 19, 90),
-                    shape: const StadiumBorder(),
-                  ),
-                  child: showprogress
-                      ? const SizedBox(
-                          height: 30,
-                          width: 30,
-                          child: CircularProgressIndicator(
-                            backgroundColor: Colors.white,
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                                Color.fromARGB(255, 237, 3, 81)),
-                          ),
-                        )
-                      : const Text(
-                          "Verify",
-                          style: TextStyle(fontSize: 20, fontFamily: "Poppins"),
-                        ),
+                height: height * .04,
+              ),
+              Text(
+                "Find Your Love💓 ",
+                style: GoogleFonts.poppins(
+                    fontSize: sizeTitle, fontWeight: FontWeight.w700),
+              ),
+              Text(
+                "Everywhere you are",
+                style: GoogleFonts.poppins(
+                    fontSize: sizeDefault, color: Colors.grey),
+              ),
+              SizedBox(
+                height: height * .04,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: primaryColor.withOpacity(.1),
+                          spreadRadius: 3,
+                          blurRadius: 12,
+                          offset: const Offset(3, 3))
+                    ],
+                    border: Border.all(width: 2, color: Colors.white),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(25),
+                    )),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  // controller: _username,
+
+                  onChanged: ((value) {
+                    // username = value;
+                  }),
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromARGB(255, 218, 218, 231)
+                          .withOpacity(.2),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Container(
+                            width: 15,
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15)),
+                            child: FluIcon(
+                              FluIcons.call,
+                              // color: Color.fromARGB(255, 54, 54, 54),
+                              color: primaryColor,
+                              style: FluIconStyles.bulk,
+                            )),
+                      ),
+                      hintText: "Phone number",
+                      hintStyle: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25)),
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          borderSide: BorderSide.none)),
                 ),
               ),
+              SizedBox(
+                height: height * .02,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: primaryColor.withOpacity(.1),
+                          spreadRadius: 3,
+                          blurRadius: 12,
+                          offset: const Offset(3, 3))
+                    ],
+                    border: Border.all(width: 2, color: Colors.white),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(25),
+                    )),
+                child: TextField(
+                  keyboardType: TextInputType.none,
+                  // controller: _username,
+
+                  onChanged: ((value) {
+                    // username = value;
+                  }),
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromARGB(255, 218, 218, 231)
+                          .withOpacity(.2),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Container(
+                            width: 15,
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15)),
+                            child: FluIcon(
+                              FluIcons.lock,
+                              // color: Color.fromARGB(255, 54, 54, 54),
+                              color: primaryColor,
+                              style: FluIconStyles.bulk,
+                            )),
+                      ),
+                      hintText: "Password",
+                      hintStyle: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25)),
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          borderSide: BorderSide.none)),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                    onPressed: () {
+                      Vibration.vibrate(amplitude: 30, duration: 30);
+                    },
+                    child: Text(
+                      'Forget password ?',
+                      style: GoogleFonts.poppins(
+                          fontSize: sizeDefault, color: primaryColor),
+                    )),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Vibration.vibrate(amplitude: 30, duration: 30);
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: height * .08,
+                  decoration: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: GoogleFonts.poppins(
+                          fontSize: sizeDefault,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: width * .15),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("You don't have an account ?",
+                        style: GoogleFonts.poppins(
+                            fontSize: sizeDefault, color: Colors.grey)),
+                    TextButton(
+                        onPressed: () {
+                          Vibration.vibrate(amplitude: 30, duration: 30);
+                        },
+                        child: Text(
+                          'Register',
+                          style: GoogleFonts.poppins(
+                              fontSize: sizeDefault, color: primaryColor),
+                        )),
+                  ],
+                ),
+              )
             ],
           ),
         ),
---
+++ lib/utils/constant.dart
@@ -0,0 +1,15 @@
+// Colors
+
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+var primaryColor = Color(0xffffc8dd);
+var primaryColorWithOpacity = Color.fromARGB(255, 255, 244, 248);
+
+// FontSizes
+var sizeTitle = 24.0;
+var sizeDefault = 14.0;
+
+// Padding
+var paddingDefault = EdgeInsets.all(20);
